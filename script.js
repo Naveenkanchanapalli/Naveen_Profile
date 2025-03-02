e81b5473c563243c2fe63bc35d221d01d7be0397
Naveenkanchanapalli <138783173+Naveenkanchanapalli@users.noreply.github.com>
2025-03-02 15:34:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const typeWriter = document.querySelector(".typewriter");
+const words = ["Python Developer", "Web Developer", "Tech Enthusiast"];
+let index = 0;
+let charIndex = 0;
+let currentWord = words[index];
+
+function typeEffect() {
+  if (charIndex < currentWord.length) {
+    typeWriter.innerHTML += currentWord[charIndex];
+    charIndex++;
+    setTimeout(typeEffect, 150);
+  } else {
+    setTimeout(eraseEffect, 2000);
+  }
+}
+
+function eraseEffect() {
+  if (charIndex > 0) {
+    typeWriter.innerHTML = currentWord.substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, 100);
+  } else {
+    index = (index + 1) % words.length;
+    currentWord = words[index];
+    setTimeout(typeEffect, 500);
+  }
+}
+
+window.onload = typeEffect;
